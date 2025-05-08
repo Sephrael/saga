f742186575cc2d7167a211015702f89a8a91df69
Dennis <dennis@houselewis.com>
2025-05-07 21:34:08 -0700

    chore: change license from MIT to Apache 2.0

--- config.py
+++ config.py
@@ -3,6 +3,20 @@
 Configuration settings for the Saga Novel Generation system.
 Centralizes constants for API endpoints, model names, file paths,
 generation parameters, validation thresholds, and logging settings.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright 2025 Dennis Lewis
 """
 
 import os
