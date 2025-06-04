bc102a4605899e4ed27cb972938662f358204ad6
dlewis3 <Lanerra@users.noreply.github.com>
2025-06-03 18:58:02 -0700

    Refactor KG maintainer

---
+++ kg_constants.py
@@ -0,0 +1,7 @@
+"""Constants used for property names in the knowledge graph."""
+
+# Relationship and node property names
+KG_REL_CHAPTER_ADDED = "chapter_added"
+KG_NODE_CREATED_CHAPTER = "created_chapter"
+KG_NODE_CHAPTER_UPDATED = "chapter_updated"
+KG_IS_PROVISIONAL = "is_provisional"
